097f9463488a1f1a100c1305f1ab8b7b9608f534
Kamarul Kawnayeen <kawnayeen.08@gmail.com>
2019-07-31 10:34:02 +0600

    'Basic Data Types' - 'List comprehension' solved

---
+++ src/_2_basic_data_types/_4_list_comprehension.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    listX = [i for i in range(0, x+1)]
+    listY = [i for i in range(0, y+1)]
+    listZ = [i for i in range(0, z+1)]
+
+    sq = [[i, j, k] for i in listX for j in listY for k in listZ if i+j+k != n]
+    print(sq)
